e8d9c54b7ab582f7e14683568b7a6ef98e9f8831
Amy Angert <angert@mail.ubc.ca>
2023-03-20 07:44:02 -0700

    script 6 tweaks

--- 6_visualizations_model_avgs.R
+++ 6_visualizations_model_avgs.R
@@ -13,7 +13,7 @@ library(cowplot)
 coeff.ALLDAT <- read.csv("data/3c_new_coefficients.csv", header = TRUE)
 
 # filter to just model average for each rarefied dataset
-coeff.avgs <- coeff.ALLDAT %>% filter(Type=="Avg") # now we have 100 model averages per species (or, <100 for the species for which some rarefied datasets threw warnings?)
+coeff.avgs <- coeff.ALLDAT %>% filter(Type=="Avg") # now we have up to 100 model averages per species (<100 for the species for which some rarefied datasets threw warnings)
 
 # split into species modeled with fire vs without
 coeffs.fire <- coeff.avgs %>% filter(Fire.Included=="Yes")
@@ -130,7 +130,7 @@ for (i in 1:dim(species.list.fire)[1]) {
   
   gg <- ggplot(graph.dat.means, aes(x = elev.vec.lin, y = preds, color = V2)) + 
     geom_line(data=graph.dat.tall, aes(group=interaction(V2, rep), color=V2), alpha=0.08, show.legend = FALSE) +
-    geom_line(size=3.5, linetype="dotted", show.legend=FALSE) +
+    geom_line(linewidth=3.5, linetype="dotted", show.legend=FALSE) +
     theme_classic() +
     scale_color_manual("Time x fire", values=col.pal.fire, labels=c("legacy", "resurvey, burned", "resurvey, unburned")) +
     scale_x_continuous(breaks=poly.ticks, labels=raw.ticks) +
@@ -145,7 +145,7 @@ for (i in 1:dim(species.list.fire)[1]) {
 # repeat last plot with legend so that legend can be saved for multi-panel fig
 gg <- ggplot(graph.dat.means, aes(x = elev.vec.lin, y = preds, color = V2)) + 
   geom_line(data=graph.dat.tall, aes(group=interaction(V2, rep), color=V2), alpha=0.08) +
-  geom_line(size=3.5, linetype="dotted") +
+  geom_line(linewidth=3.5, linetype="dotted") +
   scale_color_manual(values=col.pal.fire, labels=c("1983", "2015, burned", "2015, unburned"), guide = guide_legend(title=NULL)) +
   theme(legend.title=element_blank()) +
   theme_classic() + 
@@ -204,7 +204,7 @@ for (i in 1:dim(species.list.nofire)[1]) {
   
   gg <- ggplot(graph.dat.means, aes(x = elev.vec.lin, y = preds, color = V2)) + 
     geom_line(data=graph.dat.tall, aes(group=interaction(V2, rep), color=V2), alpha=0.08, show.legend = FALSE) +
-    geom_line(size=3.5, linetype="dotted", show.legend=FALSE) +
+    geom_line(linewidth=3.5, linetype="dotted", show.legend=FALSE) +
     theme_classic() +
     scale_color_manual(name="TIME", values=col.pal.nofire, labels=c("legacy", "resurvey")) + 
     scale_x_continuous(breaks=poly.ticks, labels=raw.ticks) +
@@ -219,7 +219,7 @@ for (i in 1:dim(species.list.nofire)[1]) {
 # repeat last plot with legend so that legend can be saved for supplemental multi-panel fig
 gg <- ggplot(graph.dat.means, aes(x = elev.vec.lin, y = preds, color = V2)) + 
   geom_line(data=graph.dat.tall, aes(group=interaction(V2, rep), color=V2), alpha=0.08) +
-  geom_line(size=3.5, linetype="dotted") +
+  geom_line(linewidth=3.5, linetype="dotted") +
   scale_color_manual(values=col.pal.nofire, labels=c("1983", "2015"), guide = guide_legend(title=NULL)) +
   theme(legend.title=element_blank()) +
   theme_classic() + 
@@ -229,29 +229,17 @@ gg <- ggplot(graph.dat.means, aes(x = elev.vec.lin, y = preds, color = V2)) +
 legend.nofire = get_legend(gg)
 
 
-#### assemble example species into multi-panel figure
-
-# first annotate % onto individual panels
-MANE.perc <- ggdraw(preds_graph_MANE) #+ draw_label("12/25\n(41%)", size=12, x=.85, y=.9, hjust=1)
-CLUN.perc <- ggdraw(preds_graph_CLUN) #+ draw_label("12/25\n(41%)", size=12, x=.85, y=.9, hjust=1)
-SPBE.perc <- ggdraw(preds_graph_SPBE) #+ draw_label("1/25\n(3%)", size=12, x=.85, y=.9, hjust=1)
-OPHO.perc <- ggdraw(preds_graph_OPHO) #+ draw_label("4/25\n(14%)", size=12, x=.85, y=.9, hjust=1)
-CHUM.perc <- ggdraw(preds_graph_CHUM) #+ draw_label("7/25\n(24%)", size=12, x=.85, y=.9, hjust=1)
-PAMY.perc <- ggdraw(preds_graph_PAMY) #+ draw_label("3/7\n(43%)", size=12, x=.85, y=.9, hjust=1)
-VAME.perc <- ggdraw(preds_graph_VAME) #+ draw_label("2/7\n(29%)", size=12, x=.25, y=.9, hjust=0)
-ARUV.perc <- ggdraw(preds_graph_ARUV) #+ draw_label("2/7\n(29%)", size=12, x=.85, y=.9, hjust=1)
+#### FIGURE 4: assemble example species into multi-panel figure
 
 # group subplots
-
 multi <- plot_grid(legend.fire,
-                   ARUV.perc, #up shift fire 
-                   VAME.perc, #expansion fire
-                   PAMY.perc, #no shift fire
-                   CHUM.perc, #down shift no fire
-                   OPHO.perc, #up shift no fire
-                   SPBE.perc, #expansion no fire
-                   #MANE.perc, #no shift no fire
-                   CLUN.perc,
+                   preds_graph_ARUV, #up shift fire 
+                   preds_graph_VAME, #expansion fire
+                   preds_graph_PAMY, #no shift fire
+                   preds_graph_CHUM, #down shift no fire
+                   preds_graph_OPHO, #up shift no fire
+                   preds_graph_SPBE, #expansion no fire
+                   preds_graph_CLUN, #no shift no fire
                    nrow=2, ncol=4,
                    labels=c("","A","B","C","D","E","F","G")) +
   theme(plot.margin = margin(50, 10, 10, 50)) #top, right, bottom, left 
@@ -266,7 +254,7 @@ multi.x <- ggdraw(add_sub(multi.labs, "Elevation (m)", size=18, x=0.5, y=0.05, h
 
 multi.xy <- ggdraw(add_sub(multi.x, "Probability of presence", size=18, x=0.02, y=2.1, angle=90))
 
-ggsave("figures/6_model_preds_multipanel.pdf", multi.xy, width=12, height=8) # too large to sync w Git; placeholder uploaded instead, but can be generated locally
+ggsave("figures/6_Figure4_model_preds_multipanel.pdf", multi.xy, width=12, height=8) # too large to sync w Git; placeholder uploaded instead, but can be generated locally
 
 
 
