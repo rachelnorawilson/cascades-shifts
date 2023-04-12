0d2263cfb688b2bf176dfbafe04751a284eded7f
Amy Angert <angert@mail.ubc.ca>
2023-04-12 06:27:24 -0700

    added t-test for difference in mean elevation of burned and unburned plot

--- S2_supp_elev_fire.R
+++ S2_supp_elev_fire.R
@@ -1,16 +1,12 @@
 # Created: May 10, 2021
-# Amended: Mar 20, 2023
+# Amended: Apr 12, 2023
 
 # This script will be used to create a violin plot of elevation vs fire history as seen in Figure S2
 
-# IMPORTANT NOTE: unless otherwise indicated, always use Understory_All.csv for these analyses as it is the ONLY file with up-to-date corrections.
-
 # Packages needed:
-
 library(tidyverse)
 
-####### PART 1: All plots regardless of species #########
-
+# Data needed:
 load("data/1.list.fires.Rda")
 burn.data <- list.fires[list.fires$Data.Type == "Resurvey",]
 burn.data$Elevation.m <- as.numeric(burn.data$Elevation.m)
@@ -26,4 +22,5 @@ violin.plot.burns <- ggplot(burn.data, aes(x = Fires, y = Elevation.m)) +
 
 ggsave("figures/FigureS2_supp_elev_fire.pdf", violin.plot.burns, width=12, height=8)
 
-
+# t-test for difference in means between unburned and burned plots
+t.test(burn.data$Elevation.m[burn.data$Fires=="Unburned"], burn.data$Elevation.m[burn.data$Fires=="Burned"])
