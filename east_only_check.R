4a58232588d1c985c1fe45c87b64850196d7908e
rachelnorawilson <rachelnora.wilson@gmail.com>
2023-06-06 20:28:19 -0700

    Checking model outcomes when subsetting to east-side plots only. Manually deleted error logs.
    Possible next step - add 3c text to generate avg coefficients for more accurate comparison.

--- east_only_check.R
+++ east_only_check.R
@@ -2,7 +2,8 @@
 
 # This script will NOT produce the final version of the analysis.
 # It is solely for determining whether subsetting to east-side plots affects
-# fire-experiencing species patterns. It is essentially a copy of script 3b.
+# fire-experiencing species patterns. It is close to a copy of script 3b.
+# However, this script differs from 3b in that it forces all species to conform to fire models.
 
 # Part of this script creates a large file and places it in a folder
 # "warning_logs"; these are not stored on GitHub but can be shared upon request and/or
@@ -15,7 +16,7 @@
 # Packages needed:
 
 library(MuMIn)
-library(plyr)
+library(dplyr)
 source("3a_dredge_log_to_df.R")
 
 # Function to create data frame of coefficients, AIC and model ID:
@@ -74,7 +75,7 @@ for(D in 1:100) {
 coeff.ALLDAT <- list()
 warn.ALLDAT <- list()
 
-for(D in 1:100) { #RUN TIME: 32 minutes 51 sec
+for(D in 1:100) { #RUN TIME: ~5 min
   
   coeff.ALLSPEC <- list()
   warn.ALLSPEC <- list()
@@ -86,7 +87,7 @@ for(D in 1:100) { #RUN TIME: 32 minutes 51 sec
   und.presence$Fires <- as.factor(und.presence$Fires)
   und.presence$Data.Type <- as.factor(und.presence$Data.Type)
   
-  for(S in 1:length(species.list)) { # RUN TIME: ~15-20 sec for 1 dataset
+  for(S in 1:length(species.list)) {
     
     # Create subset for species of interest S
     und.presence.SPEC <- subset(und.presence, Species.Code == levels(species.list)[S])
@@ -94,7 +95,8 @@ for(D in 1:100) { #RUN TIME: 32 minutes 51 sec
                                                ref="Legacy.Unburned")
     und.presence.SPEC$Fires <- relevel(und.presence.SPEC$Fires, ref="Unburned")
     und.presence.SPEC$Data.Type <- relevel(und.presence.SPEC$Data.Type, ref="Legacy")
-    und.presence.SPEC$Elevation.m2 <- und.presence.SPEC$Elevation.m^2
+    und.presence.SPEC$Elevation.m <- poly(und.presence.SPEC$Elevation.m, 2)[ , 1] #different
+    und.presence.SPEC$Elevation.m2 <- poly(und.presence.SPEC$Elevation.m, 2)[ , 2] #different
     und.presence.SPEC <- und.presence.SPEC[complete.cases(und.presence.SPEC), ] #Just in case
     
     # Emptying out previous model objects
@@ -116,7 +118,7 @@ for(D in 1:100) { #RUN TIME: 32 minutes 51 sec
     
     
     # If burn > 5, include fire as a predictor in global model (New.Data.Type):
-    if(num.burns["1", "Burned", "Resurvey"] >= 5) {
+
       mod.globfi <- glm(Pres.Abs ~ (Elevation.m + Elevation.m2) * New.Data.Type, 
                         data = und.presence.SPEC, family = "binomial", na.action = na.fail)
       options(warn = -1) # Ignore warnings - not for logging.
@@ -134,27 +136,6 @@ for(D in 1:100) { #RUN TIME: 32 minutes 51 sec
                                 dc(New.Data.Type:Elevation.m, New.Data.Type:Elevation.m2) &&
                                 dc(Elevation.m:New.Data.Type, Elevation.m2:New.Data.Type), 
                               trace = 1)
-    }
-    
-    
-    #If burn < 5 in both, exclude fire from global model:
-    if(num.burns["1", "Burned", "Resurvey"] < 5) {
-      mod.globnofi <- glm(Pres.Abs ~ Data.Type * (Elevation.m + Elevation.m2), 
-                          data = und.presence.SPEC, family = "binomial", na.action = na.fail) 
-      options(warn = -1) # Ignore warnings - not for logging.
-      dredge.list <- lapply(dredge(mod.globnofi, rank = AIC, subset = 
-                                     dc(Elevation.m, Elevation.m2) &&
-                                     dc(Data.Type:Elevation.m, Data.Type:Elevation.m2), 
-                                   trace = FALSE, evaluate = FALSE), eval)
-      names(dredge.list) <- paste("Mod", 
-                                  as.numeric(names(dredge.list)) - 1, 
-                                  sep = ".") # Converting to model ID
-      options(warn = 1) # Tell me if a model throws an error - for logging.
-      dredge.globnofi <- dredge(mod.globnofi, rank = AIC, subset = 
-                                  dc(Elevation.m, Elevation.m2) &&
-                                  dc(Data.Type:Elevation.m, Data.Type:Elevation.m2), 
-                                trace = 1)
-    }
     
     
     # Stop writing to .txt, import dataframe of warning logs
@@ -312,5 +293,177 @@ warn.ALLDAT.finaldf <- ldply(warn.ALLDAT, data.frame)
 
 
 
+###################### STOP ########################################3
 
+
+
+# Everything below is essentially a carbon copy of script 4a, used for reviewing model fitting errors
+
+# Functions needed:
+
+# Turning averaged coefficients into +/-/0
+simplify.fun <- function(varib) {
+  coeff.summary.SPEC <- coeff.summary.empty[varib]
+  dataset.vect <- as.vector(coeff.SPEC.avg$Dataset)
+  for(d in dataset.vect) {
+    if(coeff.SPEC.avg[coeff.SPEC.avg$Dataset == d, varib] > 0) {
+      coeff.summary.SPEC[d, varib] <- paste("+")
+    }
+    if(coeff.SPEC.avg[coeff.SPEC.avg$Dataset == d, varib] == 0) {
+      coeff.summary.SPEC[d, varib] <- paste("0")
+    }
+    if(coeff.SPEC.avg[coeff.SPEC.avg$Dataset == d, varib] < 0) {
+      coeff.summary.SPEC[d, varib] <- paste("-")
+    }
+  }
+  return(coeff.summary.SPEC)
+}
+
+# Summing numbers of +/-/0 for each coeff for one species
+# Empty data frame
+coeff.count.empty <- data.frame(Elevation.m = rep(NA, times = 4), 
+                                Elevation.m2 = rep(NA, times = 4), 
+                                Resurvey.Burned.fi = rep(NA, times = 4), 
+                                Resurvey.Unburned.fi = rep(NA, times = 4), 
+                                Elevation.m.Res.Burn.fi = rep(NA, times = 4), 
+                                Elevation.m.Res.Unburn.fi = rep(NA, times = 4), 
+                                Elevation.m2.Res.Burn.fi = rep(NA, times = 4), 
+                                Elevation.m2.Res.Unburn.fi = rep(NA, times = 4), 
+                                Data.Type.nofi = rep(NA, times = 4), 
+                                Data.Type.Elevation.m.nofi = rep(NA, times = 4), 
+                                Data.Type.Elevation.m2.nofi = rep(NA, times = 4),
+                                row.names = c("+", "-", 0, "Ignore"))
+
+# Collapsing 100 rows of +/-/0 into summary
+count.fun <- function(varib) {
+  df.count <- coeff.count.empty[varib]
+  if(is.na(table(simple.coeffs.SPEC[varib])["+"]) == FALSE) {
+    df.count["+", varib] <- table(simple.coeffs.SPEC[varib])["+"]
+  } 
+  if(is.na(table(simple.coeffs.SPEC[varib])["-"]) == FALSE) {
+    df.count["-", varib] <- table(simple.coeffs.SPEC[varib])["-"]
+  } 
+  if(is.na(table(simple.coeffs.SPEC[varib])["0"]) == FALSE) {
+    df.count["0", varib] <- table(simple.coeffs.SPEC[varib])["0"]
+  } 
+  if(is.na(table(simple.coeffs.SPEC[varib])["Ignore"]) == FALSE) {
+    df.count["Ignore", varib] <- table(simple.coeffs.SPEC[varib])["Ignore"]
+  } 
+  return(df.count)
+}
+
+
+#### STEP 1: Import data ####
+
+warn.ALLDAT <- warn.ALLDAT.finaldf
+
+# Can use either top_mod input (script stored outside of GitHub) or new_coefficients input
+coeff.ALLDAT <- coeff.ALLDAT.finaldf
+coeff.ALLDAT[is.na(coeff.ALLDAT)] <- paste(0)
+
+
+#### STEP 2 (OPTIONAL): Exploratory visualizations of warnings ####
+
+(numbered.species <- data.frame(Species=species.list, No.=rep(1:length(species.list))))
+
+# Choose species of interest
+warn.SPEC <- warn.ALLDAT[warn.ALLDAT$Species == "ACMI", ]
+
+# Assess  warnings
+table(warn.SPEC$Has_warning, warn.SPEC$Dataset)
+table(table(warn.SPEC$Has_warning, warn.SPEC$Dataset))
+head(warn.SPEC[warn.SPEC$Has_warning == TRUE, ])
+
+# (Optional): How did rarefaction vary between species? #
+#par(mar=c(5, 1.5, 1, 0), oma=c(0,4,0,0)) # 3-paneled graph
+#hist(coeff.SPEC$R.Occ, main = "RHAL", xlab = "No. resurvey occ", ylab = "Frequency")
+
+
+#### STEP 3: Exploring coefficients ####
+
+coeff.count.LIST <- list()
+coeff.perc.LIST <- list()
+
+for(S in 1:nrow(numbered.species)) { # Run time: 5 sec
+  #coeff.SPEC.avg <- coeff.ALLDAT[coeff.ALLDAT$Species == levels(numbered.species$Species)[S], ]
+  coeff.SPEC.avg <- coeff.ALLDAT[coeff.ALLDAT$Species == levels(numbered.species$Species)[S] & 
+                                   coeff.ALLDAT$Type == "Avg", ]
+  
+  coeff.summary.empty <- data.frame(Elevation.m = rep(NA, times = 100), 
+                                    Elevation.m2 = rep(NA, times = 100), 
+                                    Resurvey.Burned.fi = rep(NA, times = 100), 
+                                    Resurvey.Unburned.fi = rep(NA, times = 100), 
+                                    Elevation.m.Res.Burn.fi = rep(NA, times = 100), 
+                                    Elevation.m.Res.Unburn.fi = rep(NA, times = 100), 
+                                    Elevation.m2.Res.Burn.fi = rep(NA, times = 100), 
+                                    Elevation.m2.Res.Unburn.fi = rep(NA, times = 100), 
+                                    Data.Type.nofi = rep(NA, times = 100), 
+                                    Data.Type.Elevation.m.nofi = rep(NA, times = 100), 
+                                    Data.Type.Elevation.m2.nofi = rep(NA, times = 100))
+  
+  simple.coeffs.SPEC <- bind_cols(lapply(names(coeff.summary.empty), simplify.fun))
+  simple.coeffs.SPEC[is.na(simple.coeffs.SPEC)] <- paste("Ignore")
+  
+  # Summarizing the summary
+  
+  coeff.count.SPEC <- bind_cols((lapply(names(coeff.count.empty), count.fun)))
+  coeff.count.SPEC$Species <- levels(factor(coeff.SPEC.avg$Species))
+  if(length(levels(factor(coeff.SPEC.avg$Fire.Included))) == 1) {
+    coeff.count.SPEC$Fire.Included <- levels(factor(coeff.SPEC.avg$Fire.Included))
+  } else(coeff.count.SPEC$Fire.Included <- "Sometimes")
+  coeff.count.SPEC$Effect <- row.names(coeff.count.SPEC)
+  coeff.count.LIST[[S]] <- coeff.count.SPEC
+  
+  # Converting to % of good datasets
+  good.sets.SPEC <- 
+    sum(coeff.count.SPEC$Elevation.m[!coeff.count.SPEC$Effect == "Ignore"], na.rm = TRUE)
+  coeff.perc.SPEC.A <- data.frame(Species = coeff.count.SPEC$Species,
+                                  Fire.Included = coeff.count.SPEC$Fire.Included,
+                                  Total.Sets = rep(good.sets.SPEC, times = nrow(coeff.count.SPEC)),
+                                  Effect = coeff.count.SPEC$Effect, 
+                                  Elevation.m = coeff.count.SPEC$Elevation.m / good.sets.SPEC,
+                                  Elevation.m2 = coeff.count.SPEC$Elevation.m2 / good.sets.SPEC, 
+                                  Resurvey.Burned.fi = 
+                                    coeff.count.SPEC$Resurvey.Burned.fi / good.sets.SPEC, 
+                                  Resurvey.Unburned.fi = 
+                                    coeff.count.SPEC$Resurvey.Unburned.fi / good.sets.SPEC, 
+                                  Elevation.m.Res.Burn.fi = 
+                                    coeff.count.SPEC$Elevation.m.Res.Burn.fi / good.sets.SPEC, 
+                                  Elevation.m.Res.Unburn.fi = 
+                                    coeff.count.SPEC$Elevation.m.Res.Unburn.fi / good.sets.SPEC, 
+                                  Elevation.m2.Res.Burn.fi = 
+                                    coeff.count.SPEC$Elevation.m2.Res.Burn.fi / good.sets.SPEC, 
+                                  Elevation.m2.Res.Unburn.fi = 
+                                    coeff.count.SPEC$Elevation.m2.Res.Unburn.fi / good.sets.SPEC, 
+                                  Data.Type.nofi =
+                                    coeff.count.SPEC$Data.Type.nofi / good.sets.SPEC, 
+                                  Data.Type.Elevation.m.nofi =
+                                    coeff.count.SPEC$Data.Type.Elevation.m.nofi / good.sets.SPEC,  
+                                  Data.Type.Elevation.m2.nofi = 
+                                    coeff.count.SPEC$Data.Type.Elevation.m2.nofi / good.sets.SPEC)
+  coeff.perc.SPEC.B <- coeff.perc.SPEC.A[!coeff.perc.SPEC.A$Effect == "Ignore", ]
+  coeff.perc.LIST[[S]] <- coeff.perc.SPEC.B
+  
+} # End of loop
+
+coeff.count <- ldply(coeff.count.LIST, data.frame)
+coeff.perc <- ldply(coeff.perc.LIST, data.frame)
+
+#write.csv(coeff.count, 
+ #         file = "data/4a_presence_coefficients_count.csv", 
+  #        row.names = FALSE)
+
+
+#write.csv(coeff.perc.fire, 
+ #         file = "data/4a_pres_coefficients_percent_fire.csv", row.names = FALSE)
+#write.csv(coeff.perc.nofire, 
+      #    file = "data/4a_pres_coefficients_percent_NOfire.csv", row.names = FALSE)
+
+
+
+
+
+##################### STOP #######################
+
+# TBD: visualizations
 
